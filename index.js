1efa6a61d5008633d0dc731b2e6c0bff247c0d51
a.zlobin <a.zlobin@centrofinans.ru>
2020-09-19 19:05:36 +0300

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path')
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+
+
+const PORT = process.env.PORT || 3000;
+
+const index = require('./routes/main')
+const add = require('./routes/add')
+const courses = require('./routes/courses')
+
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, './views/layouts'),
+  partialsDir: [
+    path.join(__dirname, './views/partials'),
+  ]
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views')
+app.use(express.static(path.join(__dirname, "static")));
+
+
+
+app.use(index)
+app.use(add)
+app.use(courses)
+
+app.get('/', (res, req) => {
+  res.render('index')
+
+})
+app.listen(PORT, () => {
+  console.log(`server has been started on port ${PORT}`);
+})
